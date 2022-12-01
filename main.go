f480397a381314159ed27c1efa166d85afbaaa21
Loc Truong <loceducate@gmail.com>
2022-12-01 22:14:17 +0700

    config GIN

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	//dsn := "dbadmin:dbadmin@tcp(127.0.0.1:3306)/simple-rest-api?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DB_CONN_STR")
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+}
